925cbf9b5ba41fe4f2f982c201af0faadd0b7b84
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-03-05 22:46:55 +0000

    Refactored code to be object orioented and modularized

---
+++ index.js
@@ -0,0 +1,31 @@
+// Arrays to keep track of each task's state
+// const taskTitles = [];
+// const taskComplete = [];
+// const taskDescriptions = [];
+
+// Create a new task by adding to the arrays
+// A new task will be created as incomplete
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+    logState: function() {
+      console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+    },
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logState();
+task1.markCompleted();
+task1.logState();
